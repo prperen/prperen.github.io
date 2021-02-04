69949fb2f2201682f197a1eec48f2750c8084a4e
prperen <40810712+prperen@users.noreply.github.com>
2021-02-04 14:44:32 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var mylink=new Array();var a=GetQueryString("i");if(a=="me"){mylink=new Array(new link("https://wj.qq.com/","腾讯问卷"),
+	new link("https://tools.miku.ac","Miku"),
+	new link("https://netcn.console.aliyun.com/core/host/list2","主机管理"),
+new link("https://github.com/prpeilun/prpeilun.github.io","佩伦主页网站"),
+new link("https://github.com/prperen/prperen.github.io","佩伦导航网站"),
+new link("https://github.com/dsgbz/dsgbz.github.io","广播站网站"),
+new link("http://www.17sucai.com/","17 素材"),
+new link("html/newone.html?url="+window.location.href,"新链接"),
+new link("http://tools.bugscaner.com/chaojiwailian/","外链1"),
+new link("https://seo.dmeng.net/","外链2"),
+new link("http://www.6ke.com.cn/tool/","外链3"),
+new link("https://mp.weixin.qq.com","缔声"),
+new link("http://bj.96weixin.com/","公众号排版"),
+new link("http://ppt.koutu.vip/", "素材解析"),
+new link("https://www.canva.cn/","Canva"),
+new link("https://study.163.com/course/courseMain.htm?courseId=1004943019&share=1&shareId=1027183833","数据结构"))}
+all[0].linkArray=mylink;
+if(localStorage.getItem("shoucang")!=null){
+var cos=new Array();
+cos=localStorage.getItem("shoucang").split("|");
+for(var i=0;i<cos.length-1;i++){
+	all[0].linkArray.push(new link(cos[i].split("*")[0],cos[i].split("*")[1]))}
+}
+function newOne(headImg,headName,linkArray){
+	var innertext="";innertext+=("<div class='con pt'>");
+	innertext+=("<div class='title'><img src='img/icoc/"+headImg+".png'  alt='"+headName+"'/><span>"+headName+"</span></div>");
+	for(var i=0;i<linkArray.length;i++){
+		innertext+=("<a href='"+linkArray[i].linkName+"' class='btn'");
+		if(linkArray[i].name=="+"||linkArray[i].name=="-"||linkArray[i].name=="网站设置")innertext+=(" target='_self'");
+		else innertext+=(" target='_blank'");innertext+=(">"+linkArray[i].name+"</a>")}
+		innertext+=("</div>");
+		document.getElementById("con").innerHTML+=innertext
+	}
+	for(var i=0;i<all.length;i++){
+		newOne(all[i].icoc,all[i].text,all[i].linkArray)
+	}
---
+++ js/tool.js
@@ -0,0 +1,132 @@
+function ksort(vm, inputArr, sort_flags) {
+  var tmp_arr = {},
+    keys = [],
+    sorter, i, k, that = vm,
+    strictForIn = false,
+    populateArr = {};
+      sorter = function (a, b) {
+        var aFloat = parseFloat(a),
+          bFloat = parseFloat(b),
+          aNumeric = aFloat + '' === a,
+          bNumeric = bFloat + '' === b;
+        if (aNumeric && bNumeric) {
+          return aFloat > bFloat ? 1 : aFloat < bFloat ? -1 : 0;
+        } else if (aNumeric && !bNumeric) {
+          return 1;
+        } else if (!aNumeric && bNumeric) {
+          return -1;
+        }
+        return a > b ? 1 : a < b ? -1 : 0;
+      };
+  // Make a list of key names
+  for (k in inputArr) {
+    if (inputArr.hasOwnProperty(k)) {
+      keys.push(k);
+    }
+  }
+  keys.sort(sorter);
+
+  // BEGIN REDUNDANT
+  vm.php_js = vm.php_js || {};
+  vm.php_js.ini = vm.php_js.ini || {};
+  // END REDUNDANT
+  strictForIn = vm.php_js.ini['phpjs.strictForIn'] && vm.php_js.ini['phpjs.strictForIn'].local_value && vm.php_js
+    .ini['phpjs.strictForIn'].local_value !== 'off';
+  populateArr = strictForIn ? inputArr : populateArr;
+
+  // Rebuild array with sorted key names
+  for (i = 0; i < keys.length; i++) {
+    k = keys[i];
+    tmp_arr[k] = inputArr[k];
+    if (strictForIn) {
+      delete inputArr[k];
+    }
+  }
+  for (i in tmp_arr) {
+    if (tmp_arr.hasOwnProperty(i)) {
+      populateArr[i] = tmp_arr[i];
+    }
+  }
+
+  return strictForIn || populateArr;
+}
+
+function randomString(len) {
+  len = len || 32;
+  var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+  var maxPos = chars.length;
+  var pwd = '';
+  for (var i = 0; i < len; i++) {
+    pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return pwd;
+}
+
+function getFullTime(){
+    var d = new Date();
+    var y = d.getFullYear();
+    var mo = d.getMonth() + 1;
+    var date = d.getDate();
+    var w = d.getDay();
+    var ws = new Array("日", "一", "二", "三", "四", "五", "六");
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    return y+" 年 "+mo+" 月 "+date+" 日 星期"+ws[w]+" "+t(h)+"："+t(m)+"："+t(s)
+}
+
+function t(num){
+  if(num < 10)
+    return "0" + num
+  return num
+}
+function is_weixnQQ() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == "micromessenger")
+        return true;
+    if (ua.match(/QQ/i) == 'qq')
+        return true;
+    return false
+}
+/*
+var token = "?token=leCxrtexr0Bj3QfChG0g"
+var token2 = "&token=leCxrtexr0Bj3QfChG0g"*/
+var token = "?token=jrY5j7oat"
+var token2 = "&token=jrY5j7oat"
+function fanyi(ch) {
+  var en;
+  $.ajax({
+    url: "https://v1.alapi.cn/api/fanyi" + token,
+      dataType: 'JSON',
+      jsonp: 'callback',
+      type: "GET",
+      "async" : false,
+      data: {
+        q: ch,
+        to: "en"
+      },
+    success: function(re) {
+      en = re.data.trans_result[0].dst;
+    }
+  })
+  return en
+}
+
+function shuffleSelf(array, size) {
+    var index = -1,
+        length = array.length,
+        lastIndex = length - 1;
+
+    size = size === undefined ? length : size;
+    while (++index < size) {
+        // var rand = baseRandom(index, lastIndex),
+        var rand = index + Math.floor( Math.random() * (lastIndex - index + 1))
+            value = array[rand];
+
+        array[rand] = array[index];
+
+        array[index] = value;
+    }
+    array.length = size;
+    return array;
+}
